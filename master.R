a9383edb3f59a9d385fdff406d4443ee22724b87
Marie Breeur <MarieB@ndph.ox.ac.uk>
2025-02-12 17:02:19 +0000

    update parser

--- master.R
+++ master.R
@@ -8,20 +8,24 @@ parser$add_argument('--workdir', type='character', default='/gpfs3/well/travis-p
                     help='Working directory')
 parser$add_argument('--plink_path', type='character', default='/well/travis-prostate/projects/plink',
                     help='Path to PLINK executable')
-parser$add_argument('--bfile_path', type='character', default='data/LD_mat/LD_REF_DAT_MAF_MAC_Filtered',
+parser$add_argument('--bfile_path', type='character', default='gen_utils/LD_mat/LD_REF_DAT_MAF_MAC_Filtered',
                     help='Path to LD reference files')
 parser$add_argument('--respath', type='character', default='results',
                     help='Path to store results')
-parser$add_argument('--chain_path', type='character', default='data/hg19ToHg38.over.chain',
+parser$add_argument('--chain_path', type='character', default='gen_utils/hg19ToHg38.over.chain',
                     help='Path to chain file for liftover')
-parser$add_argument('--rsid38_path', type='character', default='data/hg38_rsid_map_file.txt',
+parser$add_argument('--rsid38_path', type='character', default='gen_utils/hg38_rsid_map_file.txt',
                     help='Path to GRCh38 rsID map file')
+parser$add_argument('--temp_dir_path', type='character', default="Temp",
+                    help='Path to temporary folder used for LD matrix extraction')
 
 # Trait parameters -------------------------------------------------------------
 parser$add_argument('--trait_id', type='character',
                     help='Trait identifier')
 parser$add_argument('--is_trait_eqtl_cat', type='logical', default=FALSE,
                     help='Is trait data downloaded from the eQTL catalogue')
+parser$add_argument('--is_trait_38', type='logical', default=TRUE,
+                    help='Is trait data in hg38')
 parser$add_argument('--trait_sumstats_path', type='character', 
                     help='Path to trait summary statistics')
 parser$add_argument('--trait_lbf_path', type='character', 
@@ -60,6 +64,8 @@ parser$add_argument('--out_id', type='character',
                     help='Outcome identifier')
 parser$add_argument('--is_out_eqtl_cat', type='logical', default=FALSE,
                     help='Is outcome data downloaded from the eQTL catalogue')
+parser$add_argument('--is_out_38', type='logical', default=TRUE,
+                    help='Is outcome data in hg38')
 parser$add_argument('--out_sumstats_path', type='character', 
                     help='Path to outcome summary statistics')
 parser$add_argument('--out_lbf_dir_path', type='character', 
@@ -103,23 +109,29 @@ setwd(args$workdir)
 # Rest of script remains mostly the same with variable substitutions
 .libPaths("/well/travis-prostate/users/cbe235/miniconda3/envs/renvironment/lib/R/library")
 
-library(httr)
-library(glue)
-library(jsonlite)
-library(ggrepel)
-library(ggpubr)
-library(readr)
-library(tictoc)
-library(liftOver)
-library(data.table)
-library(tidyverse)
-library(dplyr)
-library(tidyr)
-library(coloc)
-library(susieR)
-library(TwoSampleMR)
+suppressPackageStartupMessages(library(httr))
+suppressPackageStartupMessages(library(glue))
+suppressPackageStartupMessages(library(jsonlite))
+suppressPackageStartupMessages(library(ggrepel))
+suppressPackageStartupMessages(library(ggpubr))
+suppressPackageStartupMessages(library(readr))
+suppressPackageStartupMessages(library(tictoc))
+suppressPackageStartupMessages(library(liftOver))
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(tidyr))
+suppressPackageStartupMessages(library(coloc))
+suppressPackageStartupMessages(library(susieR))
+suppressPackageStartupMessages(library(TwoSampleMR))
 
 source("scripts/pipeline_utils.R")
+
+
+# Create res folder if doesn't exist
+if (!dir.exists(args$respath)) {
+  dir.create(args$respath, recursive = TRUE)
+}
 
 
 ################################################################################
@@ -131,17 +143,18 @@ if (args$is_trait_eqtl_cat){
 }else{
   trait <- fread(args$trait_sumstats_path)
   
-  build <- get_genome_build_local(trait, sampled_snps = 100, path_to_38 = args$rsid38_path,
-                                  snp_col = args$trait_snp_col,
-                                  chr_col = args$trait_chr_col,
-                                  pos_col = args$trait_pos_col)
-  if (build == "GRCH37") {
-    trait <- lift_coordinates(trait, args$chain_path,
-                              snp_col = args$trait_snp_col,
-                              chr_col = args$trait_chr_col,
-                              pos_col = args$trait_pos_col)
+  if(!args$is_trait_38){
+    build <- get_genome_build_local(trait, sampled_snps = 100, path_to_38 = args$rsid38_path,
+                                    snp_col = args$trait_snp_col,
+                                    chr_col = args$trait_chr_col,
+                                    pos_col = args$trait_pos_col)
+    if (build == "GRCH37") {
+      trait <- lift_coordinates(trait, args$chain_path,
+                                snp_col = args$trait_snp_col,
+                                chr_col = args$trait_chr_col,
+                                pos_col = args$trait_pos_col)
+    }
   }
-  
   
   trait <- format_data(data.frame(trait),
                        chr_col = args$trait_chr_col,
@@ -160,9 +173,9 @@ if (args$is_trait_eqtl_cat){
 trait <- finemap.wrapper(out = trait, out_lbf_dir_path = args$trait_lbf_path, 
                          N_out = args$N_trait, out_type = args$trait_type, out_sd = args$trait_sd,
                          plink_path = args$plink_path,
-                         bfile_path = args$bfile_path)
+                         bfile_path = args$bfile_path,
+                         temp_dir_path = args$temp_dir_path)
 
-print(colnames(trait))
 
 ################################################################################
 # Process outcome data
@@ -174,20 +187,21 @@ if(args$is_out_eqtl_cat){
 }else{
   out_raw <- fread(args$out_sumstats_path)
   
-  print(colnames(out_raw))
   # Filter and rename using raw column names
-  out_raw <- out_raw %>% 
-    filter(!!sym(args$out_chr_col) == unique(trait$chr)) 
-  
-  build <- get_genome_build_local(out_raw, sampled_snps = 100, path_to_38 = args$rsid38_path,
+  if(!args$is_out_38){
+    out_raw <- out_raw %>% 
+      filter(!!sym(args$out_chr_col) == unique(trait$chr)) 
+    
+    build <- get_genome_build_local(out_raw, sampled_snps = 100, path_to_38 = args$rsid38_path,
+                                    snp_col = args$out_snp_col,
+                                    chr_col = args$out_chr_col,
+                                    pos_col = args$out_pos_col)
+    if (build == "GRCH37") {
+      out_raw <- lift_coordinates(out_raw, args$chain_path,
                                   snp_col = args$out_snp_col,
                                   chr_col = args$out_chr_col,
                                   pos_col = args$out_pos_col)
-  if (build == "GRCH37") {
-    out_raw <- lift_coordinates(out_raw, args$chain_path,
-                                snp_col = args$out_snp_col,
-                                chr_col = args$out_chr_col,
-                                pos_col = args$out_pos_col)
+    }
   }
   
   out <- out_raw %>% filter(between(!!sym(args$out_pos_col), min(trait$pos), max(trait$pos)))
@@ -210,7 +224,8 @@ if(args$is_out_eqtl_cat){
 
 out <- finemap.wrapper(out, args$out_lbf_dir_path, args$N_out, args$out_type, args$out_sd, trait,
                        plink_path = args$plink_path,
-                       bfile_path = args$bfile_path)
+                       bfile_path = args$bfile_path,
+                       temp_dir_path = args$temp_dir_path)
 
 
 
@@ -220,20 +235,6 @@ out <- finemap.wrapper(out, args$out_lbf_dir_path, args$N_out, args$out_type, ar
 
 
 res_coloc <- coloc.wrapper(trait, out, args$trait_id, args$out_id)
-
-
-################################################################################
-# ZZ and locus plot
-################################################################################
-
-
-plots <- plot.wrapper(trait, out, res_coloc, args$trait_id, args$out_id,
-                      plink_path = args$plink_path,
-                      bfile_path = args$bfile_path)
-plotfile <- paste0(args$respath, "/plots_", 
-                   str_replace(args$trait_id, " ", "_"), "_", 
-                   str_replace(args$out_id, " ", "_"), ".pdf")
-ggsave(plotfile, plots, dpi = 300, width = 15, height = 10)
 
 
 ################################################################################
@@ -249,14 +250,31 @@ res_mr <- mr.wrapper(trait, out, args$N_out, res_coloc, args$trait_id, args$out_
 ################################################################################
 
 
-res <- list(coloc = res_coloc,
-            mr = res_mr,
-            plots = plots)
+res <- list(trait = trait,
+            out = out, 
+            coloc = res_coloc,
+            mr = res_mr)
 
 resfile <- paste0(args$respath, "/fullres_", 
                   str_replace(args$trait_id, " ", "_"), "_", 
                   str_replace(args$out_id, " ", "_"), ".rds")
 saveRDS(res, resfile)
+
+
+################################################################################
+# ZZ and locus plot
+################################################################################
+
+
+plots <- plot.wrapper(trait, out, res_coloc, res_mr, args$trait_id, args$out_id, args$out_type,
+                      plink_path = args$plink_path,
+                      bfile_path = args$bfile_path,
+                      temp_dir_path = args$temp_dir_path)
+plotfile <- paste0(args$respath, "/plots_", 
+                   str_replace(args$trait_id, " ", "_"), "_", 
+                   str_replace(args$out_id, " ", "_"), ".pdf")
+ggsave(plotfile, plots, dpi = 300, width = 15, height = 10)
+
 
 
 
