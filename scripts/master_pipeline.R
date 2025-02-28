f2554e3c378e85c24e093d8ac97bd32e5986f3ab
Marie Breeur <MarieB@ndph.ox.ac.uk>
2025-02-28 16:12:07 +0000

    update master_pipeline

--- scripts/master_pipeline.R
+++ scripts/master_pipeline.R
@@ -9,9 +9,6 @@ library(ggrepel)
 library(ggpubr)
 library(readr)
 library(tictoc)
-# library(GenomicRanges)
-# library(rtracklayer)
-# library(IRanges)
 library(liftOver)
 library(data.table)
 library(tidyverse)
@@ -22,112 +19,213 @@ library(susieR)
 library(TwoSampleMR)
 
 
-# Install packages to infer genome build if necessary
-# 
-# if (!require("BiocManager", quietly = TRUE))
-#   install.packages("BiocManager")
-# 
-# BiocManager::install("BSgenome.Hsapiens.NCBI.GRCh38")
-# BiocManager::install("SNPlocs.Hsapiens.dbSNP155.GRCh37")
-# BiocManager::install("BSgenome.Hsapiens.1000genomes.hs37d5")
-
-
-
 setwd("~/Code/Coloc_pipeline")
 source("~/Code/Coloc_pipeline/scripts/pipeline_utils.R")
 
-# Trait paths
 
-trait_id            <- "MSMB"
-trait_lbf_path      <- "data/PrC_coloc/MSMB"
-trait_sumstats_path <- "data/PrC_coloc/MSMB/MSMB_P08118_OID20275_rs10993994.rsids.csv"
-N_exp               <- 35000
-exp_type            <- "quant"
-exp_sd              <- 1
+################################################################################
+# Set parameters and paths
+################################################################################
 
-# Outcome paths and parameters
 
-out_id              <- "Prostate_cancer"
-out_sumstats_path   <- "data/PrC_coloc/ELLIPSE_V2_META_EUROPEAN_Results_012121.txt"
-out_lbf_dir_path    <- "data/PrC_coloc/prostate_cancer_lbf"
-N_out               <- 178000
-out_type            <- "cc"
-out_sd              <- 0.48
+# Main arguments ---------------------------------------------------------------
 
-# Utility paths
-
-# All the .wrapper functions have these paths as default:
-# plink_path          <- "plink"
-# bfile_path          <- "N:/EPIC_genetics/UKBB/LD_REF_FILES/LD_REF_DAT_MAF_MAC_Filtered"
-# temp_dir_path       <- "Temp"
-# They can be changed when calling the .wrappers functions
-chain_path  <- "data/hg19ToHg38.over.chain"
+workdir <- "~/Coloc_pipeline"
+plink_path <- "plink"
+bfile_path <- "N:/EPIC_genetics/UKBB/LD_REF_FILES/LD_REF_DAT_MAF_MAC_Filtered"
+chain_path <- "data/hg19ToHg38.over.chain"
 rsid38_path <- "data/hg38_rsid_map_file.txt"
-
-# Where to store the results
+temp_dir_path <- "Temp"
 respath <- "results"
 
+# Trait parameters -------------------------------------------------------------
+
+trait_id <- "MSMB"
+trait_lbf_path <- "data/PrC_coloc/MSMB"
+trait_sumstats_path <- "data/PrC_coloc/MSMB/MSMB_P08118_OID20275_rs10993994.rsids.csv"
+trait_build <- 38 # or 37, or NA
+N_trait <- 35000
+trait_type <- "quant"
+trait_sd <- 1
+# Trait column mappings
+trait_chr_col <- "chr" # Trait chromosome column name
+trait_pos_col <- "pos" # Trait position column name
+trait_snp_col <- "SNP" # Trait SNP ID column name
+trait_beta_col <- "beta" # Trait beta column name
+trait_se_col <- "se" # Trait standard error column name
+trait_pval_col <- "pval" # Trait p-value column name
+trait_log_pval <- FALSE # Trait p-values are log-transformed (TRUE/FALSE)
+trait_eaf_col <- "eaf" # Trait effect allele frequency column name
+trait_effect_allele_col <- "effect_allele" # Trait effect allele column name
+trait_other_allele_col <- "other_allele" # Trait other allele column name
+
+# Outcome parameters -----------------------------------------------------------
+
+out_id <- "Prostate_cancer"
+out_sumstats_path <- "data/PrC_coloc/ELLIPSE_V2_META_EUROPEAN_Results_012121.txt"
+out_lbf_path <- "data/PrC_coloc/prostate_cancer_lbf"
+out_build <- 38 # or 37, or NA
+N_out <- 178000
+out_type <- "cc"
+out_sd <- 0.48
+# Outcome column mappings
+out_chr_col <- "chr" # Out chromosome column name
+out_pos_col <- "pos" # Out position column name
+out_snp_col <- "SNP" # Out SNP ID column name
+out_beta_col <- "beta" # Out beta column name
+out_se_col <- "se" # Out standard error column name
+out_pval_col <- "pval" # Out p-value column name
+out_log_pval <- FALSE # Out p-values are log-transformed (TRUE/FALSE)
+out_eaf_col <- "eaf" # Out effect allele frequency column name
+out_effect_allele_col <- "effect_allele" # Out effect allele column name
+out_other_allele_col <- "other_allele" # Out other allele column name
+
+
 ################################################################################
-# Extract LBF for kidney study
+# Check parameter integrity and create folders if needed
 ################################################################################
+
+
+if (!file.exists(rsid38_path) & (is.na(trait_build) | is.na(out_build))) {
+  warning("No hg38 reference, cannot check genome builds.
+          Assuming trait and outcome are in hg38.")
+  trait_build <- out_build <- 38
+}
+
+if (!file.exists(chain_path)) {
+  warning("No chain file found, assuming trait and outcome are in hg38.")
+  trait_build <- out_build <- 38
+}
+
+
+if (!dir.exists(trait_lbf_path)) {
+  dir.create(trait_lbf_path, recursive = TRUE)
+}
+
+if (!dir.exists(out_lbf_path)) {
+  dir.create(out_lbf_path, recursive = TRUE)
+}
+
+if (!dir.exists(respath)) {
+  dir.create(respath, recursive = TRUE)
+}
+
+
+################################################################################
+# Extract LBF for trait
+################################################################################
+
+cat("Processing trait", trait_id, "\n")
+cat("\n")
 
 
 trait <- fread(trait_sumstats_path)
 
+if (is.na(trait_build)) {
+  build <- get_genome_build_local(trait,
+    sampled_snps = 100, path_to_38 = rsid38_path,
+    snp_col = trait_snp_col,
+    chr_col = trait_chr_col,
+    pos_col = trait_pos_col
+  )
+  trait_build <- ifelse(build == "GRCH37", 37, 38)
+}
+
+if (trait_build == 37) {
+  trait <- lift_coordinates(trait, chain_path,
+    snp_col = trait_snp_col,
+    chr_col = trait_chr_col,
+    pos_col = trait_pos_col
+  )
+  cat("Trait lifted to hg38.\n")
+}
+
+
 trait <- format_data(data.frame(trait),
-                   chr_col = "CHROM",
-                   pos_col = "GENPOS",
-                   snp_col = "RSID",
-                   beta_col = "BETA",
-                   se_col = "SE",
-                   pval_col = "LOG10P",
-                   log_pval = TRUE,
-                   eaf_col = "A1FREQ",
-                   effect_allele_col = "ALLELE1",
-                   other_allele_col = "ALLELE0"
+  chr_col = trait_chr_col,
+  pos_col = trait_pos_col,
+  snp_col = trait_snp_col,
+  beta_col = trait_beta_col,
+  se_col = trait_se_col,
+  pval_col = trait_pval_col,
+  log_pval = as.logical(trait_log_pval),
+  eaf_col = trait_eaf_col,
+  effect_allele_col = trait_effect_allele_col,
+  other_allele_col = trait_other_allele_col
 )
 
-trait <- finemap.wrapper(out = trait, out_lbf_dir_path = trait_lbf_path, 
-                N_out = N_exp, out_type = exp_type, out_sd = exp_sd)
+
+trait <- finemap.wrapper(
+  out = trait, out_lbf_dir_path = trait_lbf_path,
+  N_out = N_trait, out_type = trait_type, out_sd = trait_sd,
+  plink_path = plink_path,
+  bfile_path = bfile_path,
+  temp_dir_path = temp_dir_path
+)
+
+cat("Trait processed.\n")
+cat("\n")
 
 ################################################################################
-# Loading outcome summary stats
-# Run finemapping and get Bayes factors if needed
+# Process outcome data
 ################################################################################
 
 
-## Loading and formatting
+cat("Processing outcome", out_id, "\n")
+cat("\n")
+
 
 out_raw <- fread(out_sumstats_path)
 
-out_raw <- out_raw %>% 
-  filter((Chromosome == unique(trait$chr)))%>% 
-  mutate(chr = Chromosome, pos = Position, SNP = SNP_Id)
 
-build <- get_genome_build_local(out_raw, sampled_snps = 100, path_to_38 = rsid38_path)
-if (build == "GRCH37") {
-  out_raw <- lift_coordinates(out_raw, chain_path)
+if (is.na(out_build)) {
+  out_raw <- out_raw %>%
+    filter(!!sym(out_chr_col) == unique(trait$chr))
+  build <- get_genome_build_local(out,
+    sampled_snps = 100, path_to_38 = rsid38_path,
+    snp_col = out_snp_col,
+    chr_col = out_chr_col,
+    pos_col = out_pos_col
+  )
+  out_build <- ifelse(build == "GRCH37", 37, 38)
 }
 
-out <- out_raw %>% filter(between(pos, min(trait$pos), max(trait$pos)))
+if (out_build == 37) {
+  out <- lift_coordinates(out, chain_path,
+    snp_col = out_snp_col,
+    chr_col = out_chr_col,
+    pos_col = out_pos_col
+  )
+  cat("Outcome lifted to hg38.\n")
+}
 
+
+out <- out_raw %>% filter((!!sym(out_chr_col) == unique(trait$chr)) & 
+                            (between(!!sym(out_pos_col), min(trait$pos), max(trait$pos))))
 rm(out_raw)
 
 out <- format_data(data.frame(out),
-  chr_col = "chr",
-  pos_col = "pos",
-  snp_col = "SNP_Id",
-  beta_col = "Estimate_Effect",
-  se_col = "SE",
-  pval_col = "P_value",
-  log_pval = FALSE,
-  eaf_col = "EAF_Control",
-  effect_allele_col = "Allele_1",
-  other_allele_col = "Allele_2"
+  chr_col = out_chr_col,
+  pos_col = out_pos_col,
+  snp_col = out_snp_col,
+  beta_col = out_beta_col,
+  se_col = out_se_col,
+  pval_col = out_pval_col,
+  log_pval = as.logical(out_log_pval),
+  eaf_col = out_eaf_col,
+  effect_allele_col = out_effect_allele_col,
+  other_allele_col = out_other_allele_col
 )
 
 
-out <- finemap.wrapper(out, out_lbf_dir_path, N_out, out_type, out_sd, trait)
+out <- finemap.wrapper(out, out_lbf_dir_path, N_out, out_type, out_sd, trait,
+  plink_path = plink_path,
+  bfile_path = bfile_path,
+  temp_dir_path = temp_dir_path
+)
+
+cat("Outcome processed.\n")
+cat("\n")
 
 
 ################################################################################
@@ -136,6 +234,9 @@ out <- finemap.wrapper(out, out_lbf_dir_path, N_out, out_type, out_sd, trait)
 
 
 res_coloc <- coloc.wrapper(trait, out, trait_id, out_id)
+
+cat("Colocalisation performed.\n")
+cat("\n")
 
 
 ################################################################################
@@ -147,25 +248,37 @@ res_mr <- mr.wrapper(trait, out, N_out, res_coloc, trait_id, out_id)
 
 
 ################################################################################
-# ZZ and locus plot
-################################################################################
-
-
-plots <- plot.wrapper(trait, out, res_coloc, res_mr, trait_id, out_id, out_type)
-
-
-################################################################################
 # Store the results
 ################################################################################
 
 
-res <- list(coloc = res_coloc,
-            mr = res_mr,
-            plots = plots)
+res <- list(trait = trait,
+            out = out, 
+            coloc = res_coloc,
+            mr = res_mr)
 
 resfile <- paste0(respath, "/fullres_", 
                   str_replace(trait_id, " ", "_"), "_", 
                   str_replace(out_id, " ", "_"), ".rds")
 saveRDS(res, resfile)
 
+cat("Results written to", resfile, "\n")
 
+
+################################################################################
+# ZZ and locus plot
+################################################################################
+
+
+plots <- plot.wrapper(trait, out, res_coloc, res_mr, 
+                      trait_id, out_id, out_type,
+                      plink_path = plink_path,
+                      bfile_path = bfile_path,
+                      temp_dir_path = temp_dir_path)
+plotfile <- paste0(respath, "/plots_", 
+                   str_replace(trait_id, " ", "_"), "_", 
+                   str_replace(out_id, " ", "_"), ".pdf")
+ggsave(plotfile, plots, dpi = 300, width = 15, height = 10)
+
+
+cat("Plots written to", plotfile, "\n")
